f0f30e3d87e046396f3c2208f21f045a9cf99643
saksham-jain <jainsaksham16@gmail.com>
2018-05-15 16:04:09 +0530

    Add files via upload

---
+++ Social.py
@@ -0,0 +1,14 @@
+import webbrowser
+url1 = 'www.google.com'
+url2 = 'www.facebook.com'
+url3 = 'www.gmail.com'
+url4 = 'https://www.instagram.com/'
+url5 = 'https://www.linkedin.com/feed/'
+url6 = 'https://web.whatsapp.com/'
+webbrowser.open_new_tab(url1)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
+webbrowser.open_new_tab(url4)
+webbrowser.open_new_tab(url5)
+webbrowser.open_new_tab(url6)
+
---
+++ Work.py
@@ -0,0 +1,11 @@
+import webbrowser
+url1 = 'https://keep.google.com/'
+url2 = 'www.google.com'
+url3 = 'https://drive.google.com/drive/my-drive'
+url4 = 'www.gmail.com'
+url5 = 'https://github.com/'
+webbrowser.open_new_tab(url1)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
+webbrowser.open_new_tab(url4)
+webbrowser.open_new_tab(url5)
